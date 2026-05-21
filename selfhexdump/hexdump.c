96fff6c16c2d51e8ba2b5f29e99e402f678a6a67
Vaykhan <ayxansalmanov7002@gmail.com>
2026-05-22 01:10:48 +0400

    hexdump add ascii character printing and use fread

--- selfhexdump/hexdump.c
+++ selfhexdump/hexdump.c
@@ -1,30 +1,60 @@
 #include <stdio.h>
 #include <string.h>
+#include <ctype.h>
+#include <stdint.h>
 
-void print_hex(FILE* binary, int width){
-  int c;
-  int count = 0;
+int print_equivalent(char byte){
+  if(isprint((unsigned char)byte) && !isspace((unsigned char)byte)){
+    return byte;
+  }
+  else {
+    return '.';
+  }
+}
 
-  while ((c = fgetc(binary)) != EOF) {
-    count += 1;
-    //printf("Count: %d\n", count);
-    printf("%02x ", c);
+void offset_line(uint64_t offset){
+  printf("%08lx: ", offset);
+}
 
-    if(count == width){
-      printf("\n");
-      count = 0;
+void hexadecimal_line(unsigned char buffer[], size_t n, size_t width ){
+  for(size_t i = 0; i<n; i++){
+    printf("%02x ", buffer[i]);
+
+    if(i == width / 2 - 1){
+      printf(" ");
     }
   }
+    return;
+}
 
-  if(count != 0){
+void ascii_line(unsigned char buffer[], size_t n){
+  printf("|");
+  for(size_t i = 0; i<n;i++){
+    printf("%c",print_equivalent(buffer[i]));
+  }
+  printf("|");
+}
+
+
+void print_hex(FILE* binary, int width){
+  unsigned char buffer[width];
+  uint64_t n;
+  long long int offset = 0;
+
+  while((n = fread(buffer, 1, width, binary)) > 0 ){ //n is the number of bytes read
+    offset_line(offset);
+    hexadecimal_line(buffer, n, width);
+    ascii_line(buffer,n);
+    offset += width;
     printf("\n");
   }
-  return; 
+  printf("\n");
 }
-/* todo: add support for multiple files as argument*/
+
+
 int main(int argc, char *argv[]){
   
-  if(argc != 2 || (argc == 2 && strcmp(argv[1],"--help")) == 0){
+  if(argc != 2 || strcmp(argv[1],"--help") == 0){
     printf("myhexdump - make a hex dump\n\nUsage:\n\t %s [arguments]\n", argv[0]);
     return 0;
   }
@@ -34,7 +64,9 @@ int main(int argc, char *argv[]){
     perror(argv[1]);
     return 1;
   }
-  print_hex(binary, 8);
+
+
+  print_hex(binary, 16); //modify the width according to your needs
 
   return 0;
 }
