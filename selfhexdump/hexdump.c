c09dbe5363e78cfe8d5d258b9ed05d36781a8d9a
Vaykhan <ayxansalmanov7002@gmail.com>
2026-05-23 01:36:58 +0400

    no argument case

--- selfhexdump/hexdump.c
+++ selfhexdump/hexdump.c
@@ -56,11 +56,15 @@ void print_hex(FILE* binary, int width){
 
 
 int main(int argc, char *argv[]){
-  
+  if(argc == 1){
+    print_hex(stdin, 16); //use stdin if no argument provided
+    return 0;
+  }
   if(argc != 2 || strcmp(argv[1],"--help") == 0){
     printf("myhexdump - make a hex dump\n\nUsage:\n\t %s [arguments]\n", argv[0]);
     return 0;
   }
+
   
   FILE* binary = fopen(argv[1], "rb");
   if(binary == NULL){
