26e07f10d92ec40caac2c6866fcc770688e153d4
gitduck6 <warmsot19@gmail.com>
2026-06-04 23:40:17 +0400

    Commented stuff

--- selfsleep/sleep.c
+++ selfsleep/sleep.c
@@ -3,6 +3,9 @@
     * 
     * Author: gitduck6
     * Date: June 4 2026
+    * ----
+    * The software is very minimal and only takes seconds. but it does handle decimal points which is great
+    * I hope the code is readible, simplified it as much as i could
 \*/
 
 #define _DEFAULT_SOURCE
@@ -20,13 +23,13 @@ int main(int argc,char ** argv)
         usage(argv[0]);
 
     struct timespec time = {0,0};
-    char * str = *++argv;
+    char * str = argv[1];
     char is_float;
 
-    for (; isdigit(*str); str++)
+    for (;isdigit(*str); str++)
     {
-        time.tv_sec *= 10;
-        time.tv_sec += *str - '0';
+        time.tv_sec *= 10; // moves the previous digit 1 index left
+        time.tv_sec += *str - '0'; // subtracting '0' turns an ascii value to the intiger value (if it IS a digit)
     }
 
     if (*str == '\0')
@@ -35,28 +38,30 @@ int main(int argc,char ** argv)
         is_float = 1;
     else
     {
-        fprintf(stderr,"\"%s\": Only positive intigers and floats are allowed\n",*argv);
+        fprintf(stderr,"\"%s\": Only positive intigers and floats are allowed\n",argv[0]);
         return 2;
     }
 
-    str++;
-
+    
     if (is_float)
     {
+        str++; 
+
         for (int multiplier = 100000000;isdigit(*str);multiplier /= 10)
         {
-            time.tv_nsec += multiplier * (*str - '0');
+            time.tv_nsec += multiplier * (*str - '0'); // the very small values will be ignored since the multiplier will become a zero
             str++;
         }
 
         if (*str != '\0')
         {
-            fprintf(stderr,"\"%s\": Only positive intigers and floats are allowed\n",*argv);
+            fprintf(stderr,"\"%s\": Only positive intigers and floats are allowed\n",argv[0]);
             return 3;
         }
     }
 
     nanosleep(&time,NULL);
+    // finally this uses our timespec function
 
     return 0;
 
